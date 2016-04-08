590e23466cf905c7e119823350a879f3fd0ca0e9
scasado-kitmaker <scasado@kitmaker.com>
2016-04-08 16:31:34 +0200

    añadido marcar boton donde estoy

--- application/views/menu.php
+++ application/views/menu.php
@@ -23,27 +23,75 @@
 			<li>
 				<?php
 				$actual_link = 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
-				echo $actual_link;
+				$varVerde1=' ';
+				$varVerde2=' ';
+				$varVerde3=' ';
+				$varVerde4=' ';
+				$varVerde5=' ';
+				$varVerde6=' ';
+				$varVerde7=' ';
+			
 				if ($actual_link =='http://localhost/blog/index.php') {
-					
+					$varVerde1='principal';
+				}else{
+					$varVerde1=' ';
+
 				}
+				if ($actual_link =='http://localhost/blog/index.php/blog/entry/') {
+					$varVerde2='principal';
+				}else{
+					$varVerde2=' ';
+
+				}
+				if ($actual_link =='http://localhost/blog/index.php') {
+					$varVerde3='principal';
+				}else{
+					$varVerde3=' ';
+
+				}
+				if ($actual_link =='http://localhost/blog/index.php/blog/MyEntries/') {
+					$varVerde4='principal';
+				}else{
+					$varVerde4=' ';
+
+				}
+				if ($actual_link =='http://localhost/blog/index.php/blog/userDatatables/') {
+					$varVerde5='principal';
+				}else{
+					$varVerde5=' ';
+
+				}
+				if ($actual_link ==base_url().'index.php/users/signin/') {
+					$varVerde6='principal';
+				}else{
+					$varVerde6=' ';
+
+				}
+				if ($actual_link ==base_url().'index.php/users/signup/') {
+					$varVerde7='principal';
+				}else{
+					$varVerde7=' ';
+
+				}
+				
 
 				if ($this->session->userdata('is_logged_in'))
 		//echo 'Hello, '.$this->session->userdata('name').' ('. anchor(base_url()."index.php/users/logout/", "logout").') | ';
 
-					echo  anchor(base_url().'index.php/users/logout/','Hola, '.$this->session->userdata('name'). ' logout ' ,'class="principal" ');
+					echo  anchor(base_url().'index.php/users/logout/','Hola, '.$this->session->userdata('name'). ' logout ');
 
 
 
-				elseif (!$this->session->userdata('is_logged_in') && ($this->uri->segment(2) == 'signin' || $this->uri->segment(2) == 'validate'))
+				elseif (!$this->session->userdata('is_logged_in') && ($this->uri->segment(2) == 'signin' || $this->uri->segment(2) == 'principal'))
 		//echo anchor(base_url().'index.php/users/signup/','Sign Up').' | ';
-					echo anchor(base_url().'index.php/users/signup/',' Sign Up ','class="principal" ');
+					echo anchor(base_url().'index.php/users/signup/',' Sign Up ','class="'.$varVerde6.'"');
+
 
 
 
 				else
 		//echo anchor(base_url().'index.php/users/signin/','Sign In').' | ';
-					echo anchor(base_url().'index.php/users/signin/',' Entrar ','class="principal"'  );
+					echo anchor(base_url().'index.php/users/signin/',' Entrar ' ,'class="'.$varVerde7.'"');
 				?>
 
 			</li>
@@ -51,7 +99,7 @@
 			<li>
 				<?php
 				if ($this->session->userdata('is_logged_in'))
-					echo anchor(base_url().'index.php/blog/entry/',' Nueva entrada ');
+					echo anchor(base_url().'index.php/blog/entry/',' Nueva entrada ','class="'.$varVerde2.'"');
 
 				if ($this->session->userdata('is_logged_in'))
 					echo ' ';
@@ -59,7 +107,7 @@
 			</li>
 			<li>
 			<?php
-			echo anchor(base_url().' ',' Todas las entradas ');
+			echo anchor(base_url().' ',' Todas las entradas ','class="'.$varVerde3.'"');
 			if ($this->session->userdata('is_logged_in'))
 				echo ' ';
 			
@@ -71,14 +119,14 @@
 			<?php
 			if ($this->session->userdata('is_logged_in'))
 				
-				echo anchor(base_url().'index.php/blog/MyEntries/',' Mis entradas ');
+				echo anchor(base_url().'index.php/blog/MyEntries/',' Mis entradas ','class="'.$varVerde4.'"');
 			?>
 			</li>
 
 			<li>
 				<?php
 				if ($this->session->userdata('name')=='admin') {
-					echo anchor(base_url().'index.php/blog/userDatatables/',' Tablas de datos ');
+					echo anchor(base_url().'index.php/blog/userDatatables/',' Tablas de datos ','class="'.$varVerde5.'"');
 
 				}
 				?>

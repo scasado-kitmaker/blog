e513b6e081b3c41b4df17d4a54f64ca965069e89
scasado-kitmaker <scasado@kitmaker.com>
2016-04-18 15:47:52 +0200

    -Añadido el nuevo editor al creador de entradas

--- application/views/new_entry.php
+++ application/views/new_entry.php
@@ -12,6 +12,12 @@
   <script src="http://localhost/blog/public/assets/js/test.js"></script>
   <script src="//code.jquery.com/jquery-1.10.2.js"></script>
   <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+  <script src='http://localhost/blog/public/tinymce/js/tinymce/tinymce.min.js'></script>
+  <script type="text/javascript">
+    tinymce.init({
+      selector: "textarea"
+    });
+  </script>
 </head>
 <body onload="autocomplete()">
 	<div class="show_entries" id="div0">

e2d3f5c6a8ea6a9cd56455450d8b7d4aff211f56
scasado-kitmaker <scasado@kitmaker.com>
2016-04-18 13:15:49 +0200

    -Añadida libreria CKEditor para usar editor con etiquetas HTML integradas

--- application/views/edit_entry.php
+++ application/views/edit_entry.php
@@ -7,16 +7,20 @@
 	<link rel="shortcut icon" type="image/ico" href="http://localhost/blog/public/assets/images/favicon.ico"/>
 	<!--Estilos-->
 	<link href='http://localhost/blog/public/css/style.css' rel='stylesheet' type='text/css' />
+	<script type="text/javascript" src="http://localhost/blog/public/assets/ckeditor/ckeditor.js"></script>
+	<script type="text/javascript" src="http://localhost/blog/public/assets/ckfinder/ckfinder.js"></script>
 </head>
 <body>
 	<div class="show_entries">
 		<!--Inserta la vista que contiene el menu principal-->
 		<?php include('menu.php'); 		
-			$hidden = array('id' => $entry_data->id);
+		$hidden = array('id' => $entry_data->id);
 		?>
-		<!--Formulario para introducir editar la entrada-->
+
+		<!--Formulario para  editar la entrada-->
 		<?=form_open(base_url().'index.php/blog/update_entry/', '', $hidden)?>
 		<p>Título: <?=form_input('title', $entry_data->title)?></p>
+		<?php echo $this->ckeditor->editor("content",$entry_data->content);?>
 		<p>Imagen:</p>
 		<p ><?=form_input ('image', $entry_data->image)?></p>
 		<p>Contenido:</p>

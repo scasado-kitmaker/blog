da0bd9142c0c8f8920c9247af0f52031a6b9ab91
scasado-kitmaker <scasado@kitmaker.com>
2016-04-18 15:46:05 +0200

    -Cambiado editor CKEditor por TinyMCE

--- application/views/edit_entry.php
+++ application/views/edit_entry.php
@@ -7,8 +7,15 @@
 	<link rel="shortcut icon" type="image/ico" href="http://localhost/blog/public/assets/images/favicon.ico"/>
 	<!--Estilos-->
 	<link href='http://localhost/blog/public/css/style.css' rel='stylesheet' type='text/css' />
-	<script type="text/javascript" src="http://localhost/blog/public/assets/ckeditor/ckeditor.js"></script>
-	<script type="text/javascript" src="http://localhost/blog/public/assets/ckfinder/ckfinder.js"></script>
+	<!--<script type="text/javascript" src="http://localhost/blog/public/assets/ckeditor/ckeditor.js"></script>
+	<script type="text/javascript" src="http://localhost/blog/public/assets/ckfinder/ckfinder.js"></script>-->
+	<script src='http://localhost/blog/public/tinymce/js/tinymce/tinymce.min.js'></script>
+	<script type="text/javascript">
+		tinymce.init({
+			selector: "textarea"
+		});
+	</script>
+
 </head>
 <body>
 	<div class="show_entries">
@@ -18,15 +25,17 @@
 		?>
 
 		<!--Formulario para  editar la entrada-->
+		
 		<?=form_open(base_url().'index.php/blog/update_entry/', '', $hidden)?>
 		<p>Título: <?=form_input('title', $entry_data->title)?></p>
-		<?php echo $this->ckeditor->editor("content",$entry_data->content);?>
+		<!--<?php echo $this->ckeditor->editor("content",$entry_data->content);?>-->
 		<p>Imagen:</p>
 		<p ><?=form_input ('image', $entry_data->image)?></p>
 		<p>Contenido:</p>
-		<!--<p ><?=form_textarea ('content', $entry_data->content,'class="editTextArea"')?></p>-->
+		<p ><?=form_textarea ('content', $entry_data->content,'class="editTextArea"')?></p>
 		<p>Etiquetas: <?=form_input('tags', $entry_data->tags)?> (Separadas por coma)</p>
 		<?=form_submit('submit', 'Actualizar')?>
+
 	</div>
 </body>
 </html>

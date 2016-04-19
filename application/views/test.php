05965e8fa935872870208405eeaa266e007f74d3
scasado-kitmaker <scasado@kitmaker.com>
2016-04-19 15:21:48 +0200

    -Añadida vista profieler para consultar benchmarks (http://localhost/blog/index.php/profiler)

---
+++ application/views/test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> 
+<html lang = "en">
+ 
+   <head> 
+      <meta charset = "utf-8"> 
+      <title>CodeIgniter View Example</title> 
+   </head>
+	
+   <body> 
+    <div class="show_entries">
+    <?php include('menu.php');?>
+    </div>
+   </body>
+	
+</html>
